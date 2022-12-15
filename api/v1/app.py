b1265ef9d1fc08e20d3ede56cdc1f3f399a1e38d
Sebastian <sebaslopez.2002@gmail.com>
2022-12-15 09:39:23 -0500

    cors added

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,11 +4,14 @@
 
 import os
 from flask import Flask
+from flask_cors import CORS
 from models import storage
 from api.v1.views import app_views
 
 
 app = Flask(__name__)
+
+CORS(app)
 
 app.url_map.strict_slashes = False
 app.register_blueprint(app_views)
